8cf4be66be64a8940d0a7350c5ebc09b3114168b
Tayokanch <tayoquadri11@gmail.com>
2024-05-12 08:19:51 +0100

    student grading

---
+++ Array/Program.cs
@@ -0,0 +1,138 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Exercise 1
+
+//  Student Grading application that automates the calculation of grades for each student in a class. 
+//  The parameters for your application are:
+
+string yourName = Console.ReadLine().ToLower();
+int numberOfTest = 5;
+string[] studentNames = new string[] { "Sophia", "Nicolas", "Zahirah", "Jeong" };
+int[] SophiaScores = new int[] { 73, 7, 87, 8, 43, 4, 49, 5, 80, 8 };
+int[] NicolasScores = new int[] { 80, 8, 67, 6, 91, 9, 46, 4, 52, 5 };
+int[] ZahirahScores = new int[] { 52, 5, 85, 8, 69, 6, 80, 8, 72, 7 };
+int[] JeongScores = new int[] { 59, 5, 65, 6, 46, 4, 100, 10, 58, 5 };
+
+int totalScore = 0;
+List<int> studentScores = new List<int>();
+string grade = "";
+
+
+for (int i = 0; i < studentNames.Length; i++)
+{
+    if (yourName == studentNames[i].ToLower()) 
+    {
+        switch (yourName)
+        {
+            case "sophia":
+                studentScores.AddRange(SophiaScores);
+                break;
+            case "nicolas":
+                studentScores.AddRange(NicolasScores);
+                break;
+            case "zahirah":
+                studentScores.AddRange(ZahirahScores);
+                break;
+            case "jeong":
+                studentScores.AddRange(JeongScores);
+                break;
+        }
+    }
+}
+
+for (int j = 0; j < studentScores.Count; j++)
+{
+    totalScore += studentScores[j];
+}
+
+int averageScore = totalScore / numberOfTest;
+
+if (averageScore >= 97)
+{
+    grade = "A+";
+}
+else if (averageScore >= 93)
+{
+    grade = "A";
+}
+else if (averageScore >= 90)
+{
+    grade = "A-";
+}
+else if (averageScore >= 87)
+{
+    grade = "B+";
+}
+else if (averageScore >= 83)
+{
+    grade = "B";
+}
+else if (averageScore >= 80)
+{
+    grade = "B-";
+}
+else if (averageScore >= 77)
+{
+    grade = "C+";
+}
+else if (averageScore >= 73)
+{
+    grade = "C";
+}
+else if (averageScore >= 70)
+{
+    grade = "C-";
+}
+else if (averageScore >= 67)
+{
+    grade = "D+";
+}
+else if (averageScore >= 63)
+{
+    grade = "D";
+}
+else if (averageScore >= 60)
+{
+    grade = "D-";
+}
+else
+{
+    grade = "F";
+}
+
+// Print the results
+Console.WriteLine($"{yourName}\t {averageScore}\t {grade}");
+
+/*
+///Exercise 2
+///
+string[] orders = new string[] { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+foreach (string item in orders)
+{
+    if (item.StartsWith("B"))
+    {
+        Console.WriteLine($"This is {item}");
+
+    }
+
+}
+
+//Exercise 3
+// Count how mant zero(0) in this sentence
+string originalMessage = "The quick brown fox jumps over the lazy dog.";
+char[] messageChar = originalMessage.ToCharArray();
+int count = 0;
+foreach (char i in messageChar)
+{
+    if (i == 'o')
+    {
+        count++;
+    }
+};
+Console.WriteLine(count);
+*/
+
+
+
+
+
--- ifElse/Program.cs
+++ ifElse/Program.cs
@@ -46,22 +46,28 @@ Random random = new Random();
 int daysUntilExpiration = random.Next(12);
 int discountPercentage = 0;
 
-    if(10 < daysUntilExpiration){
-        return;
+
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day! Renew now and save 20 % !");
+    discountPercentage = 20;
     }
-    else if (daysUntilExpiration == 0)
-    {
-        Console.WriteLine("Your subscription has expired.");
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days. Renew now and save 10%");
+    discountPercentage = 10;
     }
-    else if (daysUntilExpiration == 1)
-    {
-        Console.WriteLine("Your subscription expires within a day! Renew now and save 20 % !");
-    }
-    else if(daysUntilExpiration >= 5){
-        Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days. Renew now and save 10%");
-    }
-    else if(daysUntilExpiration >= 10){
-        Console.WriteLine("Your subscription will expire soon. Renew now!");
-    }
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+if (discountPercentage > 0)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
+}
 
 
